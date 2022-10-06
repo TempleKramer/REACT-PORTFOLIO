08f0cec030a7f97154f92497b0694b01e3824f0b
unknown <temple0715@gmail.com>
2022-10-06 16:29:53 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React, { useState } from 'react';
+
+import About from './components/about';
+
+import ContactForm from './components/contact';
+
+function App() {
+  
+
+  return (
+    <div>
+      <main>
+
+          <>
+            <About></About>
+          </>
+
+          <ContactForm></ContactForm>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <p>
+        I am Temple Kramer and currently live in Houston, Texas. My hobbies include golf, fishing, and being a tech Junkie. I currently am enrolled in the Full Stack Web Development with the UT Bootcamp. 
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
